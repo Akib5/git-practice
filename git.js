d064512bbb091348337e793b6f76e87fd4dda46c
Akib5 <106296630+Akib5@users.noreply.github.com>
2024-08-29 18:56:07 +0600

    Update git.js

--- git.js
+++ git.js
@@ -1,1 +1,76 @@
 
+ 
+//01
+
+let car = "volvo" ;
+
+console.log(car);
+
+/*
+//02
+
+let firstName = "John" ;
+let lastName = "Doe" ;
+let age = 35;
+let fullDetails = firstName + " " + lastName +" "+ age ;
+ console.log(fullDetails);{
+*/
+
+/*
+//03
+
+let x = 10;
+let y = 5;
+x = x * y;
+console.log(x)
+
+*/
+
+
+
+//04
+let length = 16; // number data type ? 
+let lastName = "Johnson"; // string data type ? 
+
+const x = {
+  firstName: "John",  
+  lastName: "Doe"
+};
+
+//05 
+
+function myFunction() {
+  alert("Hello World!");
+}
+
+console.log(myFunction());
+
+
+//06
+
+let person ={
+
+  name: "john",
+  age:50,
+}
+console.log(person.name +" is "+ person.age)
+
+
+
+
+
+//09
+
+let randomNumber = Math.random();
+console.log(randomNumber);
+
+ let largestNumber = Math.max(10, 20);
+console.log(largestNumber);
+
+ let squareRoot = Math.sqrt(9);
+console.log(squareRoot);
+
+
+
+
+
